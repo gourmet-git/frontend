32b3ed52601b4b58773135bb7028ede4b77b214a
Diane Zhou <diane@commure.com>
2021-06-17 12:47:10 -0700

    Add dish submission form

--- src/app/dish-form/dish-form.component.ts
+++ src/app/dish-form/dish-form.component.ts
@@ -5,23 +5,29 @@ import { NgbActiveModal, NgbModal } from "@ng-bootstrap/ng-bootstrap";
 @Component({
   selector: "dish-form-content",
   templateUrl: "./dish-form-content.html",
+  styleUrls: ["./dish-form.component.scss"],
 })
 export class DishFormContent {
-  checkoutForm = this.formBuilder.group({
-    name: "",
-    address: "",
+  dish = this.formBuilder.group({
+    dish: "",
+    photo: "",
+    num_recipes: "",
   });
 
   constructor(
     public activeModal: NgbActiveModal,
     private formBuilder: FormBuilder
   ) {}
+
+  onSubmit(): void {
+    console.warn("Your dish has been submitted!", this.dish.value);
+    this.dish.reset();
+  }
 }
 
 @Component({
   selector: "dish-form-component",
   templateUrl: "./dish-form.component.html",
-  styleUrls: ["./dish-form.component.scss"],
 })
 export class DishFormComponent {
   constructor(private modalService: NgbModal) {}
