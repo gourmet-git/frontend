43fce3c4705171808a78cb40935563044294cb5e
Diane Zhou <diane@commure.com>
2021-06-17 14:28:29 -0700

    Move recipe submission inside dish

--- src/app/submission-form/submission-form.component.ts
+++ src/app/submission-form/submission-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 import { FormArray, FormBuilder } from "@angular/forms";
 import { NgbActiveModal, NgbModal } from "@ng-bootstrap/ng-bootstrap";
 
@@ -8,18 +8,18 @@ import { NgbActiveModal, NgbModal } from "@ng-bootstrap/ng-bootstrap";
   styleUrls: ["./submission-form.component.scss"],
 })
 export class SubmissionFormContent {
+  public dish!: string;
+  constructor(
+    public activeModal: NgbActiveModal,
+    private formBuilder: FormBuilder
+  ) {}
+
   recipe = this.formBuilder.group({
-    dish: "",
     source: "",
     serving_size: "",
     ingredients: this.formBuilder.array([this.formBuilder.control("")]),
     instructions: this.formBuilder.array([this.formBuilder.control("")]),
   });
-
-  constructor(
-    public activeModal: NgbActiveModal,
-    private formBuilder: FormBuilder
-  ) {}
 
   get ingredients() {
     return this.recipe.get("ingredients") as FormArray;
@@ -38,6 +38,7 @@ export class SubmissionFormContent {
   onSubmit(): void {
     console.warn("Your recipe has been submitted!", this.recipe.value);
     this.recipe.reset();
+    this.activeModal.close("Close click");
   }
 }
 
@@ -46,9 +47,11 @@ export class SubmissionFormContent {
   templateUrl: "./submission-form.component.html",
 })
 export class SubmissionFormComponent {
+  @Input() dish = "";
   constructor(private modalService: NgbModal) {}
 
-  open() {
+  open(dish: string) {
     const modalRef = this.modalService.open(SubmissionFormContent);
+    modalRef.componentInstance.dish = dish;
   }
 }
